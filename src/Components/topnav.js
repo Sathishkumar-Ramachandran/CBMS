084237a781ed816cd7df41c47bd962be0199a497
Sathishkumar-Ramachandran <sathish.spacy2001@gmail.com>
2023-01-24 16:10:55 +0530

    setting top navigation bar

--- src/Components/topnav.js
+++ src/Components/topnav.js
@@ -1,11 +1,33 @@
 import React, {useState} from "react";
+import { Spin as Hamburger } from 'hamburger-react';
+import { Link } from "react-router-dom";
 
 import Leftnav from "./leftnav";
+import "../styles/dashboard.css";
 
 const Topnav = () => {
     const [state, setState] = useState(''); 
+    const [isOpen, setOpen] = useState(false);
+
     return(
-        <>
-        </>
+        <nav>
+            <Hamburger toggled={isOpen} toggle={setOpen} className='Hamburger' />
+        <ul>
+            S
+            <li>
+                <Link to='' className="" />
+            </li>
+            <li>
+                <Link to='' className="" />
+            </li>
+            <li>
+                <Link to='' className="" />
+            </li>
+            <li>
+                <Link to='' className="" />
+            </li>
+        </ul>
+
+        </nav>
     )
 }
--- src/Pages/dashboard.js
+++ src/Pages/dashboard.js
@@ -1,9 +1,13 @@
 import React from "react";
 
 
+import Leftnav from "../Components/leftnav";
+
 const Dashboard = () => {
     return(
         <>
+            <Leftnav />
+            
         </>
     )
 }
