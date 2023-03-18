2c50d5a7a966501f5c388336ffdfaa755b49b303
Sathishkumar-Ramachandran <sathish.spacy2001@gmail.com>
2023-03-18 11:48:10 +0530

    Conflict Cleared

--- src/App.js
+++ src/App.js
@@ -17,14 +17,10 @@ import Admin from "./Components/Admin.js";
 import Dropdown from "./Components/submenu.js";
 import Googleadmin from "./Pages/Google/admingoogle.js";
 import Mediasetup from "./Pages/Google/Mediasetup.js";
-<<<<<<< HEAD
-import Account from "./Components/admin/accountsetup.js";
-import Campaignform from "./Pages/Google/campaignform.js";
-=======
+
 import Campaignform from "./Pages/Google/campaignform.js";
 
 
->>>>>>> 8c7f0ce8ade59b8f5127d88bd44a07a225c9c03f
 
 function App() {
   function WithNavs() {
@@ -56,7 +52,7 @@ function App() {
           <Route path='/admin' element={<Admin />} />
 
 
-          <Route path="/admin/account" element={<Account />} />
+          {/* <Route path="/admin/account" element={<Account />} /> */}
         
           <Route path='/admin/google/' element={<Googleadmin />} />
           <Route path='/admin/google/accountsetup/accounts' element={""} />
@@ -64,18 +60,9 @@ function App() {
           <Route path='/admin/google/accountsetup/roles' element={""} />
           <Route path='/admin/google/accountsetup/groups' element={""} />
           <Route path='/admin/google/accountsetup/formfields' element={""} />
-<<<<<<< HEAD
-          <Route path='/admin/google/campaignsetup/campaignform' element={""} />
-=======
           <Route path='/admin/google/campaignsetup/campaigns' element={""} />
           <Route path='/admin/google/campaignform' element={<Campaignform />} />
-<<<<<<< HEAD
->>>>>>> 3f7a5289aff47b0a9b86f38f638a714b45a8e97e
-          <Route path='/admin/google/campaignsetup/createcampaigns' element={""} />
-          <Route path='/admin/google/campaignsetup/formfields' element={<Campaignform />} />
-=======
           <Route path='/admin/google/campaignsetup/formfields' element={""} />
->>>>>>> 8c7f0ce8ade59b8f5127d88bd44a07a225c9c03f
           <Route path='/admin/google/mediasetup' element={<Mediasetup />} />
           
           </Route>
--- src/Pages/Google/campaignform.js
+++ src/Pages/Google/campaignform.js
@@ -1,18 +1,7 @@
 import React from "react";
 import Toolkit from "../../Components/Formfields/toolkit";
-<<<<<<< HEAD
-import "../../styles/Formfields/toolkit.css"
-const Campaignform = () => {
-    return(
-        <>
-        <Toolkit />
-        </>
-    )
-}
- 
 
-export default Campaignform;
-=======
+
 
 
 const Compaignform = () => {
@@ -24,4 +13,4 @@ const Compaignform = () => {
 }
 
 export default Compaignform;
->>>>>>> 8c7f0ce8ade59b8f5127d88bd44a07a225c9c03f
+
