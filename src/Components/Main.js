906e500ddbce4cde279570488529132407d9ad7b
PRAVEENRAJ PANNEERSELVAM <praveenraj272000@gmail.com>
2023-03-21 22:08:12 +0530

    Dashbaord Add-On

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -3,7 +3,9 @@ import React from 'react'
 import '../styles/Main.css';
 import Linechart from './Charts/Linechart';
 import PieRechart from './Charts/PieRechart';
-import Dashboardtable from './Charts/Dashboardtable'
+import Dashboardtable from './Charts/Dashboardtable';
+import Map from './Maps/Map';
+
 const Main = () => {
   return (
     <>
@@ -36,7 +38,10 @@ const Main = () => {
 <PieRechart className='piechart'/>
 <Linechart/>
 </div>
-{/* <Dashboardtable/> */}
+<div className='maps-main'>
+<Dashboardtable />
+<Map/>
+</div>
 </>
 
 
